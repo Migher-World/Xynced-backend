334458b4b820bfd534adaf5334e1d0b20850ff5e
ayorcodes <adeayo35@gmail.com>
2022-09-21 09:41:20 +0100

    added caching

--- src/modules/redis-cache/redis-cache.service.ts
+++ src/modules/redis-cache/redis-cache.service.ts
@@ -2,33 +2,45 @@ import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
 import { Cache, CachingConfig } from 'cache-manager';
 import { AppEvents } from '../../constants';
-import RedisStore from '../../shared/plugins/redis/redis';
 
 @Injectable()
 export class RedisCacheService {
   constructor(@Inject(CACHE_MANAGER) private cache: Cache) {}
 
+  async getFromCacheWithFallback<T>(
+    key: string,
+    fallback: () => T,
+    options?: CachingConfig,
+  ) {
+    const cached = await this.getFromCache(key);
+    if (cached) return cached;
+    const data = await fallback();
+    await this.set(key, data, options);
+    return data;
+  }
+
+  async getFromCache(key: string) {
+    const data = await this.get(key);
+    if (!data.status) return null;
+    return data.data;
+  }
+
   async get(key: string) {
     const data = await this.cache.get(key);
     if (!data) return { status: false };
     return { status: true, data };
-
-    // return RedisStore.get(key);
   }
 
   set(key: string, value: any, options?: CachingConfig) {
     return this.cache.set(key, value, options);
-    // return RedisStore.set(key, value, options);
   }
 
   remove(key: string) {
     return this.cache.del(key);
-    // return RedisStore.remove(key);
   }
 
   @OnEvent(AppEvents.STORE_IN_CACHE)
   async setFromEvent(payload: any) {
-    console.log({ payload });
     const { key, value, options } = payload;
     await this.cache.set(key, value, options);
   }
